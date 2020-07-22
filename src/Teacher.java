68b580552059704e808721687c8059c73fc1e9e5
mbartlett01 <67392004+mbartlett01@users.noreply.github.com>
2020-07-21 21:15:59 -0500

    Finish Teacher class

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,80 +1,109 @@
 
-public class Student{
+public class Teacher
+{
 
     /**
      * Declare all the variables as mentioned in the UML
     **/
-	
+	private int id;
+	private String name;
+	private int salary;
+	private int salaryEarned;
+
 
     /**
      * Write the constructor as given in the UML
-    **/
-
-    /**
-     * return the number of id of the student
-    **/
-
-
-    /**
-     * add a student's id to the school
-    **/
-
-
-
-    /**
-     * return the name of the student in the school
-    **/
-
-
-    /**
-     * add the student name to the school
-    **/
-
-
-    /**
-     * @return the fees paid by the student
-    **/
-
-
-	/**
-     * @return the total fees of the student
-    **/
-
-
-
-    /**
      *
-     * @return the grade of the student.
     **/
+	public Teacher(int id, String name, int salary)
+	{
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
 
     /**
-     * Used to update the student's grade.
-     * @param grade new grade of the student.
+     * return the number of id of the teacher
+     * 
+     * @return the id number of the teacher
     **/
+	public int getId()
+	{
+		return id;
+	}
+
+    /**
+     * add a teacher's id to the school
+     * 
+     * @param id the new id number
+    **/
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+    /**
+     * return the name of the teachers in the school
+     * 
+     * @return the name of the teacher
+    **/
+	public String getName()
+	{
+		return name;
+	}
+
+    /**
+     * add the teacher name to the school
+     * 
+     * @param name the new name of the teacher
+    **/
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
 
     /**
-     * Keep adding the fees to feesPaid Field.
-     * Add the fees to the fees paid.
-     * The school is going receive the funds.
-     *
-     * @param fees the fees that the student pays.
+     * return the salary of the teachers in the school
+     * 
+     * @return the salary of the teacher
     **/
+	public int getSalary()
+	{
+		return salary;
+	}
 
 
     /**
-     *
-     * @return the remaining fees.
+     * add the salary of teacher to the school
+     * 
+     * @param salary the new salary of the teacher
     **/
+	public void setSalary(int salary)
+	{
+		this.salary = salary;
+	}
+
+
+    /**
+     * Adds to salaryEarned.
+     * Removes from the total money earned by the school.
+     * @param salary the amount paid to the teacher
+    **/
+	public void receiveSalary(int salary)
+	{
+		salaryEarned += salary;
+	}
 
 
     /**
      * Display method
     **/
 
-    public String displayInfo(){
-        return "Name of the Student: " + name +
-                " Remaining fees to be paid: $" + getRemainingFees();
+    public String displayInfo()
+    {
+        return "Name of the teacher: " + name +
+                " Total salary earned so far: $" + salaryEarned;
     }
 }
